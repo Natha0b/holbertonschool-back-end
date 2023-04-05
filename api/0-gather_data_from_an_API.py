c5c6dae73499c7afc70c0e0f5fb0b130ff6d298a
Natha0b <laura.barona04@gmail.com>
2023-04-05 17:29:12 -0500

    add changes in the code file 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,27 +6,26 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python employee_todo.py EMPLOYEE_ID")
-        sys.exit(1)
 
-    employee_id = sys.argv[1]
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
 
-    response = requests.get(f"https://jsonplaceholder.typicode.com/users/\
-                            {employee_id}")
-    employee_data = response.json()
+    employee_id = int(sys.argv[1])
 
-    response = requests.get(f"https://jsonplaceholder.typicode.com/\
-                            todos?userId={employee_id}")
-    todos_data = response.json()
+    response = requests.get(f'{BASE_URL}/todos?userId={employee_id}')
+    todos = response.json()
 
-    num_completed_tasks = sum(1 for todo in todos_data if todo["completed"])
-    total_num_tasks = len(todos_data)
+    num_completed_tasks = sum(todo['completed'] for todo in todos)
+    total_num_tasks = len(todos)
 
-    print(f"Employee {employee_data['name']} is done with tasks \
-          ({num_completed_tasks}/{total_num_tasks}):")
-    for todo in todos_data:
-        if todo["completed"]:
-            print(f"\t [x] {todo['title']}")
-        else:
-            print(f"\t [ ] {todo['title']}")
+    try:
+        response = requests.get(f'{BASE_URL}/users/{employee_id}')
+        employee_name = response.json()['name']
+    except requests.exceptions.RequestException:
+        employee_name = 'Unknown'
+
+    print(f'Employee {employee_name} is done with tasks(\
+          {num_completed_tasks}/{total_num_tasks}):')
+
+    for todo in todos:
+        if todo['completed']:
+            print(f'\t {todo["title"]}')
