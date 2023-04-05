741c6e85c20eed2139b6091b08c2b1224ae05018
Natha0b <laura.barona04@gmail.com>
2023-04-05 16:47:52 -0500

    add changes in the style files

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-"""Script that given employee ID, returns information"""
+'''Script that given employee ID, returns information'''
+
+if __name__ == '__main__':
+    import requests
+    import sys
 
 
-import requests
-import sys
-
-
-"""API endpoint URL"""
+'''API endpoint URL'''
 BASE_URL = 'https://jsonplaceholder.typicode.com'
 
-"""Get employee ID from command line argument"""
+'''Get employee ID from command line argument'''
 employee_id = int(sys.argv[1])
 
-"""Send GET request to API endpoint for employee's todos"""
 response = requests.get(f'{BASE_URL}/todos?userId={employee_id}')
 
-"""Extract JSON data from response"""
+'''Extract JSON data from response'''
 todos = response.json()
 
-"""Count number of completed tasks and total number of tasks"""
 num_completed_tasks = sum(todo['completed'] for todo in todos)
 total_num_tasks = len(todos)
 
-"""Get employee name from API endpoint"""
 response = requests.get(f'{BASE_URL}/users/{employee_id}')
 employee_name = response.json()['name']
 
-"""Print employee TODO list progress"""
+'''Print employee TODO list progress'''
 print(f'Employee {employee_name} is done with\
       tasks({num_completed_tasks}/{total_num_tasks}):')
 
-""" Print title of completed tasks """
+'''Print title of completed tasks'''
 for todo in todos:
     if todo['completed']:
         print(f'\t {todo["title"]}')
