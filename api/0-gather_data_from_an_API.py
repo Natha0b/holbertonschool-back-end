9cac8c42d5f3012074365df13cef824bf35d9a87
Natha0b <laura.barona04@gmail.com>
2023-04-05 17:23:59 -0500

    add changes in the code file 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,16 +12,21 @@ if __name__ == "__main__":
 
     employee_id = sys.argv[1]
 
-    response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    response = requests.get(f"https://jsonplaceholder.typicode.com/users/\
+                            {employee_id}")
     employee_data = response.json()
 
-    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    response = requests.get(f"https://jsonplaceholder.typicode.com/\
+                            todos?userId={employee_id}")
     todos_data = response.json()
 
     num_completed_tasks = sum(1 for todo in todos_data if todo["completed"])
     total_num_tasks = len(todos_data)
 
-    print(f"Employee {employee_data['name']} is done with tasks ({num_completed_tasks}/{total_num_tasks}):")
+    print(f"Employee {employee_data['name']} is done with tasks \
+          ({num_completed_tasks}/{total_num_tasks}):")
     for todo in todos_data:
         if todo["completed"]:
-            print(f"\t {todo['title']}")
+            print(f"\t [x] {todo['title']}")
+        else:
+            print(f"\t [ ] {todo['title']}")
