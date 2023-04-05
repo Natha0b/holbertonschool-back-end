0894d5219dc55f431e5cfc6ce83fb5eedb39e888
Natha0b <laura.barona04@gmail.com>
2023-04-05 14:32:59 -0500

    add changes in the style files

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,21 +10,28 @@ import sys
 # API endpoint URL
 BASE_URL = 'https://jsonplaceholder.typicode.com'
 
+# Get employee ID from command line argument
 employee_id = int(sys.argv[1])
 
+# Send GET request to API endpoint for employee's todos
 response = requests.get(f'{BASE_URL}/todos?userId={employee_id}')
 
+# Extract JSON data from response
 todos = response.json()
 
+# Count number of completed tasks and total number of tasks
 num_completed_tasks = sum(todo['completed'] for todo in todos)
 total_num_tasks = len(todos)
 
+# Get employee name from API endpoint
 response = requests.get(f'{BASE_URL}/users/{employee_id}')
 employee_name = response.json()['name']
 
+# Print employee TODO list progress
 print(f'Employee {employee_name} is done with\
       tasks({num_completed_tasks}/{total_num_tasks}):')
 
+# Print title of completed tasks
 for todo in todos:
     if todo['completed']:
         print(f'\t {todo["title"]}')
--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,26 +7,35 @@ import requests
 import sys
 import csv
 
+# API endpoint URL
 BASE_URL = 'https://jsonplaceholder.typicode.com'
 
+# Get employee ID from command line argument
 employee_id = int(sys.argv[1])
 
+# Send GET request to API endpoint for employee's todos
 response = requests.get(f'{BASE_URL}/todos?userId={employee_id}')
 
+# Extract JSON data from response
 todos = response.json()
 
+# Get employee name from API endpoint
 response = requests.get(f'{BASE_URL}/users/{employee_id}')
 employee_name = response.json()['username']
 
+# Define CSV file name
 csv_file_name = f'{employee_id}.csv'
 
+# Open CSV file for writing
 with open(csv_file_name, mode='w', newline='') as csv_file:
     # Create CSV writer
     writer = csv.writer(csv_file, delimiter=',')
 
+    # Write CSV header row
     writer.writerow(['USER_ID', 'USERNAME',
                      'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
 
+    # Write CSV data rows
     for todo in todos:
         writer.writerow([employee_id, employee_name,
                          todo['completed'], todo['title']])
