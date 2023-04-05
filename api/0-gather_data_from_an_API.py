842bff15cbe49e6eb3c3fe01c22b7e0b43ebb5a3
Natha0b <laura.barona04@gmail.com>
2023-04-05 11:31:14 -0500

    add information in the file 0 and 1

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that given employee ID, returns information
+about his/her TODO list progress
+"""
+import requests
+import sys
+
+# API endpoint URL
+BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+employee_id = int(sys.argv[1])
+
+response = requests.get(f'{BASE_URL}/todos?userId={employee_id}')
+
+todos = response.json()
+
+num_completed_tasks = sum(todo['completed'] for todo in todos)
+total_num_tasks = len(todos)
+
+response = requests.get(f'{BASE_URL}/users/{employee_id}')
+employee_name = response.json()['name']
+
+print(f'Employee {employee_name} is done with tasks({num_completed_tasks}/{total_num_tasks}):')
+
+for todo in todos:
+    if todo['completed']:
+        print(f'\t {todo["title"]}')
---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script to export data in the CSV format.
+"""
+
+import requests
+import sys
+import csv
+
+BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+employee_id = int(sys.argv[1])
+
+response = requests.get(f'{BASE_URL}/todos?userId={employee_id}')
+
+todos = response.json()
+
+response = requests.get(f'{BASE_URL}/users/{employee_id}')
+employee_name = response.json()['username']
+
+csv_file_name = f'{employee_id}.csv'
+
+with open(csv_file_name, mode='w', newline='') as csv_file:
+    # Create CSV writer
+    writer = csv.writer(csv_file, delimiter=',')
+
+    writer.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
+
+    for todo in todos:
+        writer.writerow([employee_id, employee_name, todo['completed'], todo['title']])
+
+    print(f'{csv_file_name} has been created successfully.')
