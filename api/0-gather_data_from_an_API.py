4319f2ee7c35ff30f4b48514c646da8496888097
Natha0b <laura.barona04@gmail.com>
2023-04-07 12:09:38 -0500

    add changes in the file 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,14 +10,14 @@ if __name__ == '__main__':
         print("Usage: python3 {} EMPLOYEE_ID".format(sys.argv[0]))
         sys.exit(1)
 
-    employee_id = sys.argv[1]
+    EMPLOYEE_ID = sys.argv[1]
 
     # Make API request to get employee's TODO list
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    tasks_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{EMPLOYEE_ID}")
+    tasks_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={EMPLOYEE_ID}")
 
     if user_response.status_code != 200 or tasks_response.status_code != 200:
-        print("Failed to get TODO list for employee", employee_id)
+        print("Failed to get TODO list for employee", EMPLOYEE_ID)
         sys.exit(1)
 
     user = user_response.json()
@@ -25,16 +25,16 @@ if __name__ == '__main__':
 
     # Count completed tasks
     completed_tasks = [task for task in tasks if task['completed']]
-    num_completed_tasks = len(completed_tasks)
+    NUMBER_OF_DONE_TASKS = len(completed_tasks)
 
     # Count total tasks
-    num_total_tasks = len(tasks)
+    TOTAL_NUMBER_OF_TASKS = len(tasks)
 
     # Get employee name
-    employee_name = user['name']
+    EMPLOYEE_NAME = user['name']
 
     # Print progress report
-    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{num_total_tasks}):")
+    print(f"Employee {EMPLOYEE_NAME} is done with tasks ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
 
     # Print completed tasks
     for task in completed_tasks:
